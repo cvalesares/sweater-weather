780df8724d329e21fe518f4a4e6a7418ea314924
Christian Valesares <cvalesares@gmail.com>
2022-01-16 14:57:49 -0500

    remove unused code from backgrounds#index

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,6 +2,5 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     photo = UnsplashFacade.photo(params[:location]).first
     render json: {data: {type: 'image', id: 'null', attributes: { image: photo}}}
-    #render json: BackgroundSerializer.new(UnsplashFacade(params[:location]))
   end
 end
