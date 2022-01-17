445679139e11afd6aac8e1745525a19db02d047a
Christian Valesares <cvalesares@gmail.com>
2022-01-17 13:10:15 -0500

    add route endpoint to mapquest service

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -5,10 +5,19 @@ RSpec.describe MapquestService do
     expect(MapquestService.conn).to be_a Faraday::Connection
   end
 
-  it 'parses JSON data into a hash', :vcr do
+  it 'can find a location', :vcr do
     address = MapquestService.place("Denver,Co")
 
     expect(address).to be_a Hash
     expect(address).to have_key :results
   end
+
+  it 'can find a route', :vcr do
+    route = MapquestService.route("New York, NY", "Los Angeles, CA")
+
+    expect(route).to be_a Hash
+    expect(route).to have_key :route
+  end
+
+  #maybe add another it block for route sad path here
 end
