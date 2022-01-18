84e6a611b07f0a92d14a91708151bbf6a6491120
Christian Valesares <cvalesares@gmail.com>
2022-01-17 21:13:42 -0500

    add edge case for api_key validation on road_trip#create

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Roadtrip Request' do
   it 'can create a roadtrip', :vcr do
+    user = User.create!(email: 'pleasework@gmail.com', password: 'password123', password_confirmation: 'password123', api_key: 'raahahfpmmvtcvhzlztulkqwjcuernyjjbzwzzisqrdovaxfrz')
+
     roadtrip_params = {
       origin: "Denver, CO",
       destination: "Pueblo, CO",
@@ -16,7 +18,23 @@ RSpec.describe 'Roadtrip Request' do
     expect(road_trip[:data][:attributes][:start_city]).to eq("Denver, CO")
     expect(road_trip[:data][:attributes][:end_city]).to eq("Pueblo, CO")
     expect(road_trip[:data][:attributes][:travel_time]).to eq("01:45:23")
-    expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to eq(51.67)
+    expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to eq(48.2)
     expect(road_trip[:data][:attributes][:weather_at_eta][:conditions]).to eq("few clouds")
+  end
+
+  it 'returns an error if invalid api_key', :vcr do
+    user = User.create!(email: 'pleasework@gmail.com', password: 'password123', password_confirmation: 'password123', api_key: 'raahahfpmmvtcvhzlztulkqwjcuernyjjbzwzzisqrdovaxfrz')
+
+    roadtrip_params = {
+      origin: "Denver, CO",
+      destination: "Pueblo, CO",
+      api_key: "randomapikeyguess"
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/road_trip", headers: headers, params: JSON.generate(roadtrip_params)
+
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Please enter valid credentials")
   end
 end
